8f70893da2d0464efa2a1c82fe8ee5c2eddb39a1
Jonas Otten <jonadev95@posteo.org>
2021-08-04 21:36:11 +0000

    update worker

--- project.clj
+++ project.clj
@@ -1,12 +1,12 @@
-(defproject example-worker "0.1.0-SNAPSHOT"
+(defproject {{ project-name }} "0.1.0-SNAPSHOT"
   :description "Example Clojure Cloudflare worker"
   :plugins [[lein-cljsbuild "1.1.8"]]
   :dependencies [[org.clojure/clojure "1.10.3"]
                  [org.clojure/clojurescript "1.10.339"]
-                 [org.clojure/core.async "1.3.618"]]
+                 [org.clojure/core.async "1.3.618"]
+                 [com.github.sauercrowd/clojureworker "0.0.1"]]
   :source-paths ["src"]
   :hooks [leiningen.cljsbuild]
-  :resource-paths ["../target/clojureworker-0.0.1.jar"]
   :cljsbuild {
     :builds {:production
                {:source-paths ["src"]
